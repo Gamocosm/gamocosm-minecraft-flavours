4bf7cb8869a5ad5f7df8188bfbbb0621b026f556
MarMed <medina.mario45@gmail.com>
2015-01-27 01:27:13 -0500

    Made recommended changes

--- craftbukkit.sh
+++ craftbukkit.sh
@@ -4,14 +4,15 @@ set -e
 
 URL="https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
 CRAFTBUKKIT_INSTALLER="BuildTools.jar"
-CRAFTBUKKIT_JAR="BuildTools/CraftBukkit/target/craftbukkit-1.8-R0.1-SNAPSHOT.jar"
+CRAFTBUKKIT_JAR="BuildTools/craftbukkit-1.8.jar"
 MINECRAFT_DIR="/home/mcuser/minecraft"
 
+rm -rf BuildTools
 mkdir BuildTools
-cd BuildTools
-wget -O BuildTools.jar "$URL"
+pushd BuildTools
+wget -O "$CRAFTBUKKIT_INSTALLER" "$URL"
 java -jar "$CRAFTBUKKIT_INSTALLER"
-cd "$MINECRAFT_DIR"
+popd
 mv "$CRAFTBUKKIT_JAR" minecraft_server-run.jar
 echo "eula=true" > eula.txt
 echo "enable-query=true" > server.properties
--- spigot.sh
+++ spigot.sh
@@ -4,14 +4,15 @@ set -e
 
 URL="https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
 SPIGOT_INSTALLER="BuildTools.jar"
-SPIGOT_JAR="BuildTools/Spigot/Spigot-Server/target/spigot-1.8-R0.1-SNAPSHOT.jar"
+SPIGOT_JAR="BuildTools/spigot-1.8.jar"
 MINECRAFT_DIR="/home/mcuser/minecraft"
 
+rm -rf BuildTools
 mkdir BuildTools
-cd BuildTools
-wget -O BuildTools.jar "$URL"
+pushd BuildTools
+wget -O "$SPIGOT_INSTALLER" "$URL"
 java -jar "$SPIGOT_INSTALLER"
-cd "$MINECRAFT_DIR"
+popd
 mv "$SPIGOT_JAR" minecraft_server-run.jar
 echo "eula=true" > eula.txt
 echo "enable-query=true" > server.properties
