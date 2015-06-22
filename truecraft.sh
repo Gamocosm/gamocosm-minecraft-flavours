784a5856fffabedda2c6059c51056e45388ed1a8
Raekye <adrian@creatifcubed.com>
2015-06-22 18:32:35 -0400

    read stdin in truecraft wrapper script

--- truecraft.sh
+++ truecraft.sh
@@ -21,11 +21,21 @@ mkdir extracted
 unzip "$TRUECRAFT_ARCHIVE"
 rm "$TRUECRAFT_ARCHIVE"
 
-cat << EOF > minecraft_server-run.sh
+cat << 'EOF' > minecraft_server-run.sh
 #!/bin/sh
-mono TrueCraft.exe
+mono TrueCraft.exe &
+TRUECRAFT_PID="$!"
+
+while read -r LINE
+do
+	if [ "$LINE" == "stop" ]; then
+		break
+	fi
+done
+
+kill "$TRUECRAFT_PID"
 EOF
-chmod +x minecraft_server-run.sh
+chmod u+x minecraft_server-run.sh
 
 cp -r . "$WD"
 cd ..
