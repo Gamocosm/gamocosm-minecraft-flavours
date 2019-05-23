2ea69538e139208ba5c60b1e9aaa8152b2c3a100
logwet <sachila.herath@outlook.com>
2019-05-23 13:06:22 +1000

    Added support for new Mojang asset system

--- vanilla.sh
+++ vanilla.sh
@@ -7,7 +7,18 @@
 set -e
 
 VANILLA_VERSION="$MINECRAFT_FLAVOUR_VERSION"
-URL="https://s3.amazonaws.com/Minecraft.Download/versions/$VANILLA_VERSION/minecraft_server.$VANILLA_VERSION.jar"
+
+MANIFEST="https://launchermeta.mojang.com/mc/game/version_manifest.json"
+URL=$(python3 -c "
+from json import load
+from urllib.request import urlopen
+from sys import stdout
+manifest=load(urlopen('$MANIFEST'))
+version='$VANILLA_VERSION'
+if '$VANILLA_VERSION'=='latest': version=manifest['latest']['release']
+url=[index['url'] for index in manifest['versions'] if index['id']==version][0]
+stdout.write(str(load(urlopen(url))['downloads']['server']['url']))
+")
 
 rm -rf minecraft_server-run.jar
 wget -O minecraft_server-run.jar "$URL"
