6e111f4a86c8d2c4460c4a3634f21b2fa7b112b1
Raekye <Raekye@users.noreply.github.com>
2020-04-06 06:03:22 -0400

    open udp ports for bedrock

--- vanilla_bedrock.sh
+++ vanilla_bedrock.sh
@@ -16,3 +16,7 @@ rm -rf bedrock-server-1.14.32.1.zip
 echo '#!/bin/sh' > minecraft_server-run.sh
 echo "LD_LIBRARY_PATH=. ./bedrock_server" >> minecraft_server-run.sh
 chmod u+x minecraft_server-run.sh
+firewall-cmd --add-port=19132/udp
+firewall-cmd --add-port=19132/udp --permanent
+firewall-cmd --add-port=19133/udp
+firewall-cmd --add-port=19133/udp --permanent
