bc4450cd9ceb996ed0212686a73bc7b16883e6f6
joplasoft <joplasoft@gmail.com>
2021-01-14 21:18:15 +0100

    fix: forge

--- forge.sh
+++ forge.sh
@@ -8,12 +8,10 @@ set -e
 
 FORGE_VERSION="$MINECRAFT_FLAVOUR_VERSION"
 FORGE_INSTALLER="forge-$FORGE_VERSION-installer.jar"
-FORGE_UNIVERSAL="forge-$FORGE_VERSION-universal.jar"
 URL="http://files.minecraftforge.net/maven/net/minecraftforge/forge/$FORGE_VERSION/$FORGE_INSTALLER"
 
 rm -f "$FORGE_INSTALLER"
 wget -O "$FORGE_INSTALLER" "$URL"
 java -jar "$FORGE_INSTALLER" --installServer
-mv "$FORGE_UNIVERSAL" minecraft_server-run.jar
 echo "eula=true" > eula.txt
 echo "enable-query=true" > server.properties
