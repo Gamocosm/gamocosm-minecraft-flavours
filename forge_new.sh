5227f9d9c4c3ac578cd19e7b811a8a3ebfc16bde
joplasoft <joplasoft@gmail.com>
2021-01-14 22:33:09 +0100

    fix: forge 1.13+

--- forge_new.sh
+++ forge_new.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # script author: Raekye
-# server: Minecraft Forge
+# server: Minecraft Forge 1.13+
 # project developers: Minecraft Forge team
 # project website: http://minecraftforge.net
 
@@ -8,12 +8,12 @@ set -e
 
 FORGE_VERSION="$MINECRAFT_FLAVOUR_VERSION"
 FORGE_INSTALLER="forge-$FORGE_VERSION-installer.jar"
-FORGE_UNIVERSAL="forge-$FORGE_VERSION-universal.jar"
+FORGE_SERVER="forge-$FORGE_VERSION.jar"
 URL="http://files.minecraftforge.net/maven/net/minecraftforge/forge/$FORGE_VERSION/$FORGE_INSTALLER"
 
 rm -f "$FORGE_INSTALLER"
 wget -O "$FORGE_INSTALLER" "$URL"
 java -jar "$FORGE_INSTALLER" --installServer
-mv "$FORGE_UNIVERSAL" minecraft_server-run.jar
+mv "$FORGE_SERVER" minecraft_server-run.jar
 echo "eula=true" > eula.txt
 echo "enable-query=true" > server.properties
